cf41384d52c8088689915ae3e1a282db5893b3a2
Emmanuel-Benigno-Gonzalez <benigno.az214@gmail.com>
2025-08-11 22:33:09 -0600

    Cambio de la variable PORT

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import server from './server'
 import colors from 'colors'
 import { SERVER_PORT } from './config'
 
-const port = parseInt(SERVER_PORT) || 4000
+//const port = parseInt(SERVER_PORT) || 4000
+const port = process.env.PORT || SERVER_PORT || 4000
 
 server.listen(port, () => {
     console.log( colors.cyan.bold(`REST API en el puerto ${port}`))
