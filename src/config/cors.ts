98d0c6724c39718af88b7b459ca82d2c4b3dbb25
Emmanuel-Benigno-Gonzalez <benigno.az214@gmail.com>
2025-08-13 17:09:43 -0600

    Configuracion a db.ts y cors.ts para deploy

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,7 +3,7 @@ import cors, { CorsOptions } from 'cors'
 
 export const corsOptions: CorsOptions = {
     origin: function (origin, callback) {
-        const whitelist = [FRONTEND_URL];
+        const whitelist = [FRONTEND_URL,undefined]; //NOTA: EN PRODUCCION QUITAR EL undefined
 
         if (process.argv[2] === '--api') {
             whitelist.push(undefined)
@@ -12,6 +12,7 @@ export const corsOptions: CorsOptions = {
         if (whitelist.includes(origin)) {
             callback(null, true);
         } else {
+            console.log('Origen bloqueado:', origin); 
             callback(new Error('Error de Cors'));
         }
     }
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,17 @@
 import { Sequelize } from 'sequelize-typescript'
 import { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DATABASE_URL } from '../config'
 
+const pathModels =
+  process.env.NODE_ENV === 'production'
+    ? __dirname + '/../models/**/*.js'  // PRODUCCION
+    : __dirname + '/../models/**/*.ts'; // DESARROLLO
+
+
 //const db = DATABASE_URL
 const db = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
-      models: [__dirname + '/../models/**/*.ts'],
+      //models: [__dirname + '/../models/**/*.ts'],
+      models: [pathModels],
       dialect: 'mysql', // o el que uses
       dialectOptions: {
         ssl: {
@@ -21,7 +28,7 @@ const db = process.env.DATABASE_URL
       host: DB_HOST,
       port: parseInt(DB_PORT),
       dialect: 'mysql', // o el que uses
-      models: [__dirname + '/../models/**/*.ts'],
+      models: [pathModels],
       logging: true,
     })
 
